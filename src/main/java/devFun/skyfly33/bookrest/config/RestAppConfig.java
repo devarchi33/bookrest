caf82cf6b3aed24b409cc8d6e13860c1dce79180
skyfly33 <skyfly33727@gmail.com>
2015-08-08 16:05:42 +0900

    complete java base spring configuration.

---
+++ src/main/java/devFun/skyfly33/bookrest/config/RestAppConfig.java
@@ -0,0 +1,11 @@
+package devFun.skyfly33.bookrest.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by donghoon on 15. 8. 8..
+ */
+
+@Configuration
+public class RestAppConfig {
+}
---
+++ src/main/java/devFun/skyfly33/common/config/AppConfig.java
@@ -0,0 +1,13 @@
+package devFun.skyfly33.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by donghoon on 15. 8. 8..
+ *
+ * database, transaction, ,DAO, service management config.
+ */
+
+@Configuration
+public class AppConfig {
+}
