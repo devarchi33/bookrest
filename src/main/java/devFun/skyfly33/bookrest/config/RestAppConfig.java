c2989c56f37c0b9b0d3655cfeef6c069607ba311
skyfly33 <skyfly33727@gmail.com>
2015-08-10 01:56:01 +0900

    add json messageConverter.

--- src/main/java/devFun/skyfly33/bookrest/config/RestAppConfig.java
+++ src/main/java/devFun/skyfly33/bookrest/config/RestAppConfig.java
@@ -3,11 +3,16 @@ package devFun.skyfly33.bookrest.config;
 import devFun.skyfly33.common.domain.Book;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by donghoon on 15. 8. 8..
@@ -16,8 +21,20 @@ import java.util.Date;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"devFun.skyfly33.bookrest.controller"},
-        useDefaultFilters = false, includeFilters = {@ComponentScan.Filter(Controller.class)})
-public class RestAppConfig {
+        useDefaultFilters = false, includeFilters = {@Filter(Controller.class)})
+public class RestAppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureMessageConverters(
+            List<HttpMessageConverter<?>> converters) {
+        converters.add(mappingJackson2HttpMessageConverter());
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        return converter;
+    }
 
     @Bean
     public Book getDummyBook() {
