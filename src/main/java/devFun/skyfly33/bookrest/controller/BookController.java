042ea83cd55822abc715deff883ecee5adf55580
skyfly33 <skyfly33727@gmail.com>
2015-08-10 01:56:32 +0900

    add controller method && Controller unit test case.

--- src/main/java/devFun/skyfly33/bookrest/controller/BookController.java
+++ src/main/java/devFun/skyfly33/bookrest/controller/BookController.java
@@ -3,11 +3,11 @@ package devFun.skyfly33.bookrest.controller;
 import devFun.skyfly33.common.domain.Book;
 import devFun.skyfly33.common.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by donghoon on 15. 8. 9..
@@ -20,10 +20,45 @@ public class BookController {
     @Autowired
     BookService bookService;
 
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public List<Book> getBooks() {
+        List<Book> books = bookService.getBooks();
+        return books;
+    }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
     public String getBook(@PathVariable("id") Long id) {
         Book book = bookService.getBook(id);
         return String.format("결과값은 %s 입니다.", String.valueOf(book));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public Book createBook(@RequestBody Book book) {
+        bookService.createBook(book);
+        Book selectedBook = bookService.getBook(book.getId());
+        return selectedBook;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Book updateBook(@PathVariable("id") Long id, @RequestBody Book book) {
+        bookService.updateBook(book);
+        Book selectedBook = bookService.getBook(book.getId());
+        return selectedBook;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public Book deleteBook(@PathVariable("id") Long id) {
+        bookService.deleteBook(id);
+        Book deleteBook = new Book();
+        deleteBook.setId(id);
+        return deleteBook;
     }
 }
--- src/test/java/devFun/skyfly33/bookrest/controller/BookControllerTest.java
+++ src/test/java/devFun/skyfly33/bookrest/controller/BookControllerTest.java
@@ -1,7 +1,10 @@
 package devFun.skyfly33.bookrest.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import devFun.skyfly33.bookrest.config.RestAppConfig;
 import devFun.skyfly33.common.config.AppConfig;
+import devFun.skyfly33.common.domain.Book;
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,7 +21,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import java.util.Date;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -46,10 +54,93 @@ public class BookControllerTest {
     }
 
     @Test
-    public void testBook() throws Exception {
+    public void testGetBooks() throws Exception {
         MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
-                .get("/books/10")
-                .accept(MediaType.valueOf("text/plain;charset=UTF-8"));
-        this.mockMvc.perform(requestBuilder).andDo(print()).andExpect(status().isOk());
+                .get("/books")
+                .accept(MediaType.APPLICATION_JSON);
+
+        this.mockMvc.perform(requestBuilder).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", Matchers.hasSize(3)))
+                .andExpect(jsonPath("$[0].id", is(2)))
+                .andExpect(jsonPath("$[0].title", is("b")))
+                .andExpect(jsonPath("$[0].creator", is("b")))
+                .andExpect(jsonPath("$[1].id", is(10)))
+                .andExpect(jsonPath("$[1].title", is("스칼라 프로그래밍")))
+                .andExpect(jsonPath("$[1].creator", is("이동훈")));
+
     }
+
+    @Test
+    public void testGetBook() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/books/2")
+                .accept(MediaType.APPLICATION_JSON);
+
+        this.mockMvc.perform(requestBuilder).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(2)))
+                .andExpect(jsonPath("$.title", is("b")))
+                .andExpect(jsonPath("$.creator", is("b")));
+    }
+
+    @Test
+    public void testCreateBook() throws Exception {
+        String content = "{\"id\" : 11, \"title\":\"바라야 내전\", " +
+                "\"creator\":\"로이스 맥마스터 부졸드\", \"type\":\"외국 판타지 소설\",\"date\":131337846000}";
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/books")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content)
+                .accept(MediaType.APPLICATION_JSON);
+
+        this.mockMvc.perform(requestBuilder).andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void testUpdateBook() throws Exception {
+        Book updateBook = new Book(10L, "AngularJS", "이동훈", "자바스크립트", new Date());
+
+
+//        Java 2 Json
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(updateBook);
+        logger.debug("content = {}", content);
+
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put("/books/3")
+                .contentType(MediaType.APPLICATION_JSON).content(content)
+                .characterEncoding("UTF-8")
+                .accept(MediaType.APPLICATION_JSON);
+
+        this.mockMvc.perform(requestBuilder).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id", is(3)))
+                .andExpect(jsonPath("$.title", is("AngularJS")))
+                .andExpect(jsonPath("$.creator", is("이동훈")));
+    }
+
+    @Test
+    public void testDeleteBook() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/books/100")
+                .accept(MediaType.APPLICATION_JSON);
+
+        this.mockMvc.perform(requestBuilder).andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(3)));
+    }
+
+//    @Test
+//    public void testBook() throws Exception {
+//        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+//                .get("/books/10")
+//                .accept(MediaType.valueOf("text/plain;charset=UTF-8"));
+//        this.mockMvc.perform(requestBuilder).andDo(print()).andExpect(status().isOk());
+//    }
 }
